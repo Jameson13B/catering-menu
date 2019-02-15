034f248e899c4d5a4e435691ef6b23720640ea93
Jameson13B <jamesonbrown@neolivity.com>
2019-02-15 14:30:29 -0700

    Updated styling and layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { setGlobal } from "reactn";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./components/App";
-import * as serviceWorker from "./serviceWorker";
+
+setGlobal({
+  mains: [],
+  sides: [],
+  sauces: []
+});
 
 ReactDOM.render(
   <Router>
@@ -11,8 +16,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
